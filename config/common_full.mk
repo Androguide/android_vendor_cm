711858d4947eb8d2937f91ffc9fd94401aac9ba6
Androguide <louisteboul@gmail.com>
2013-06-10 16:55:09 +0200

    Big cleanup of useless dependencies & overlays - part 3 (Vendor/CM)

---
+++ config/common_full.mk
@@ -0,0 +1,15 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/cm/config/common.mk)
+
+include ubuntu/assets/UbuntuAssets.mk
+
+# Optional CM packages
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam
